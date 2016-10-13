751fc4331e857d13ee3e473b9c8f1ded21bc4ba3
Kaitlyn <kzucchi@gmail.com>
2016-10-12 22:46:39 -0700

    Add files via upload
    Fixed the bug in the code from the original file. 
    New file is funny_food_joke.py with correct code.
    24 files to add to the folder "funny_food_joke" and store on documents directory on a mac for the program to run

--- FunnyFoodJoke.py
+++ FunnyFoodJoke.py
@@ -1,21 +1,26 @@
 #Download the folder "Why is the mushroom always invited to parties?"
 #Save folder to desktop
 #Run program to unscramble pictures and reveal the answer to a funny food joke
+
+#this code has a bug in the part to rename files or print file list. I haven't found the bug yet but it's not working!
+#test the code here with a print function to see if ANYTHING works in this code!
+print("program started")
+
 import os
+
+
 def rename_files():
     #(1) get file names from a folder
-    file_list = os.listdir(r"C:/Users/user/Desktop/Why is the mushroom always invited to parties?")
-    #print(file_list)
+    file_list = os.listdir("/Users/user/Documents/Funny_Food_Joke")
+    print(file_list)
     saved_path = os.getcwd()
     print("Current Working Directory is "+saved_path)
-    os.chdir(r"C:/Users/user/Desktop/Why is the mushroom always invited to parties?")
-
-    #this code has a bug in the part to rename files or print file list. I haven't found the bug yet but it's not working!
-
-    #(2) for each file, rename filename
+    os.chdir("/Users/user/Documents/Funny_Food_Joke")
+    #(2) for each file, rename file name
     for file_name in file_list:
         os.rename(file_name, file_name.translate(None, "0123456789") )
     os.chdir(saved_path)
 
 rename_files()
     
+print("end of program")
